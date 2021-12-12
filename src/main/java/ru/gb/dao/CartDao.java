4d09bd918efef9397d555a38c020d0b4ea7ff6f7
aleksejleonov <typical_drilling@mail.ru>
2021-12-12 20:00:17 +0500

    Changes

---
+++ src/main/java/ru/gb/dao/CartDao.java
@@ -0,0 +1,8 @@
+package ru.gb.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.gb.entity.Cart;
+
+public interface CartDao extends CrudRepository<Cart, Long> {
+
+}
---
+++ src/main/java/ru/gb/dao/ManufacturerDao.java
@@ -0,0 +1,11 @@
+package ru.gb.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.gb.entity.Manufacturer;
+
+import java.util.List;
+
+public interface ManufacturerDao  extends CrudRepository<Manufacturer, Long> {
+    List<Manufacturer> findAll();
+    Manufacturer findById();
+}
